def6608a89402a2e00049a0d7973f0eb2618035e
Benj1bear <131564813+Benj1bear@users.noreply.github.com>
2025-01-26 13:17:25 +1300

    Update custom_generator.py

--- gcopy/custom_generator.py
+++ gcopy/custom_generator.py
@@ -940,15 +940,26 @@ class Generator(Pickler):
         if loops:
             blocks,linetable,temp_lineno=[],[],self.gi_frame.f_lineno ## for 0 based indexing ##
             start_pos,end_pos=loops.pop()
-            temp_block,reference_indent,indexes=self._source_lines[temp_lineno:end_pos],get_indent(self._source_lines[start_pos]),list(range(temp_lineno,end_pos))
             ## adjustment ##
-            temp_block,indexes=loop_adjust(*control_flow_adjust(temp_block,indexes,reference_indent),self._source_lines[start_pos:end_pos],*(start_pos,end_pos))
+            temp_block,indexes=loop_adjust(
+                *control_flow_adjust(
+                    self._source_lines[temp_lineno:end_pos],  # temp_block
+                    list(range(temp_lineno,end_pos)),         # indexes
+                    get_indent(self._source_lines[start_pos]) # reference_indent
+                ),
+                self._source_lines[start_pos:end_pos],        # outer_loop
+                *(start_pos,end_pos)                          # pos
+            )
             ## update ##
             blocks+=temp_block;linetable+=indexes;temp_lineno=end_pos
-            for start_pos,end_pos in loops:
-                temp_block,reference_indent,indexes=self._source_lines[temp_lineno:end_pos],get_indent(self._source_lines[start_pos]),list(range(temp_lineno,end_pos))
+            for start_pos,end_pos in reversed(loops):
                 ## adjustments ##
-                temp_block,indexes=loop_adjust(temp_block,indexes,self._source_lines[start_pos:end_pos],*(start_pos,end_pos))
+                temp_block,indexes=loop_adjust(
+                    self._source_lines[temp_lineno:end_pos],
+                    list(range(temp_lineno,end_pos)),
+                    self._source_lines[start_pos:end_pos],
+                    *(start_pos,end_pos)
+                )
                 ## update ##
                 blocks+=temp_block;linetable+=indexes;temp_lineno=end_pos
                 print(f"{temp_lineno,start_pos,end_pos,indexes=}")
