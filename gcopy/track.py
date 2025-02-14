9856b2a7b751e53f5f0aadba3e5d48376a0942c9
Benj1bear <131564813+Benj1bear@users.noreply.github.com>
2025-02-15 04:38:22 +1300

    Update track.py

--- gcopy/track.py
+++ gcopy/track.py
@@ -1,16 +1,17 @@
 ################
 ### tracking ###
 ################
-from source_processing import is_definition, lineno_adjust, get_indent
-from utils import is_cli, get_history_item, get_col_offset
+from .source_processing import is_definition, lineno_adjust, get_indent
+from .utils import is_cli, get_history_item, get_col_offset
 from inspect import currentframe, getframeinfo
 
 ## for the monkey patching ##
 from fishhook import hook, orig  ## pip install fishhook
 from typing import Iterator, Iterable
+from types import FrameType
 
 
-def track_iter(obj: Iterator | Iterable) -> Iterator | Iterable:
+def track_iter(obj: Iterator | Iterable, frame: FrameType) -> Iterator | Iterable:
     """
     Tracks an iterator in the local scope initiated by a for loop
 
@@ -31,7 +32,6 @@ def track_iter(obj: Iterator | Iterable) -> Iterator | Iterable:
     Using in generator expressions uses the col_offset instead
     """
     obj = iter(obj)
-    frame = currentframe().f_back
     if frame.f_code.co_name == "<genexpr>":
         ## we don't need to concern about interference with indent adjusts since the frames ##
         ## are separate  e.g. we don't need to include the line and the offset just the offset ##
@@ -45,13 +45,15 @@ def track_iter(obj: Iterator | Iterable) -> Iterator | Iterable:
         ## won't work for compound statements that are in block statements ##
         ## therefore, we check for a block statement and add 4 if so ##
         temp = code_context[key:]
-        if (
-            temp.startswith("if ")
-            or temp.startswith("for ")
-            or temp.startswith("while ")
-            or is_definition(temp)
-        ) and lineno_adjust(frame.f_code, frame) == 0:
-            key += 4
+        ## needs fixing e.g. lineno_adjust somewhere ##
+        ## also why does it not have other block conditions?? e.g. except, elif,case,default etc. ##
+        # if (
+        #     temp.startswith("if ")
+        #     or temp.startswith("for ")
+        #     or temp.startswith("while ")
+        #     or is_definition(temp)
+        # ) and lineno_adjust(frame) == 0:
+        #     key += 4
     frame.f_locals[".%s" % key] = obj
     return obj
 
@@ -76,7 +78,7 @@ def offset_adjust(f_locals: dict) -> dict:
         if isinstance(key, str) and key[0] == "." and key[1:].isdigit():
             del f_locals[key]
             lineno += 1
-            f_locals[4 * lineno] = value
+            f_locals[".%s" % (4 * lineno)] = value
     return f_locals
 
 
@@ -89,8 +91,8 @@ def hook_iter(iterator: Iterator | Iterable) -> None:
         exec("class %s(%s):pass" % (name, name), globals())
 
         def __iter__(self) -> Iterator:
-            f_locals = currentframe().f_back.f_locals
-            return track_iter(iterator, f_locals)
+            frame = currentframe().f_back
+            return track_iter(iterator, frame)
 
         globals()[name].__iter__ = __iter__
     except:
@@ -98,14 +100,20 @@ def hook_iter(iterator: Iterator | Iterable) -> None:
         @hook(iterator)
         def __iter__(self) -> Iterator:
             iterator = orig(self)
-            f_locals = currentframe().f_back.f_locals
-            return track_iter(iterator, f_locals)
+            frame = currentframe().f_back
+            return track_iter(iterator, frame)
 
 
-#############################
-#### patch all iterators ####
-#############################
-## Note: Can't change syntactical initiations e.g. (,), [], {}, and {...:...} ##
-for name, obj in vars(__builtins__).items():
-    if isinstance(obj, type) and issubclass(obj, Iterator | Iterable):
-        hook_iter(obj)
+def patch_iterators():
+    #############################
+    #### patch all iterators ####
+    #############################
+    ## Note: Can't change syntactical initiations e.g. (,), [], {}, and {...:...} ##
+    if isinstance(__builtins__, dict):
+        objs = __builtins__.items()
+    else:
+        objs = vars(__builtins__).items()
+    for name, obj in objs:
+        if isinstance(obj, type) and issubclass(obj, Iterator | Iterable):
+            ## for some reason 'range' is not working??
+            hook_iter(obj)
