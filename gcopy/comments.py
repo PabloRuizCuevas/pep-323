df502bf020fcd5c8c46246c800106b4a5177ef11
Benj1bear <131564813+Benj1bear@users.noreply.github.com>
2025-02-18 23:22:42 +1300

    Update comments.py

--- gcopy/comments.py
+++ gcopy/comments.py
@@ -4,32 +4,40 @@ TODO:
 1. general testing and fixing to make sure everything works before any more changes are made
 
     Finish fixing:
-     - unpack - fix ordering of list popping when doing recursion
-     - get_instructions .positions needs implementing for versions < 3.11 if possible (might not be)
-     - lineno_adjust
-     - check track_iter and probably add more conditional checks?
-
-    Needs checking:
-    - check all the documentation + docstrings
-    - f_back on frame needs checking
-    - check lineno_adjust to ensure that it's robust, not sure if it works in all cases.
-      It relies on a single line containing all the code, it might be possible that you
-      can have multiple independent expressions in one line but I haven't checked. -
-      This function is only to help with users that choose to use compound statements.
-
-    - check overwrite in __init__
-
-    - check __init__ on string and code object initialisation options
+        Priority:
+         tests and fixing code e.g. source_processing + custom_generator:
+         Extracting source is not a major priority e.g. get the Generator working first
+         e.g. all the code adjusters are the priorty essentially
     
-    - check .send on generator expressions and in general for those that don't use it
+        Needs checking:
+    
+        - check overwrite in __init__
+    
+        - check __init__ on string and code object initialisation options
+        
+        - check .send on generator expressions and in general for those that don't use it
+    
+        - check throw
+    
+        - check that the returns work now e.g. using next(self) and for i in self: ...
 
-    - check throw
 
-    - check that the returns work now e.g. using next(self) and for i in self: ...
-
-
-    control_flow_adjust - test to see if except does get included as a first line of a state (it shouldn't)
-    need to test what happens when there are no lines e.g. empty lines or no state / EOF
+        control_flow_adjust - test to see if except does get included as a first line of a state (it shouldn't)
+        need to test what happens when there are no lines e.g. empty lines or no state / EOF
+    
+        Do after:
+        - unpack - fix ordering of list popping when doing recursion
+        - lineno_adjust can be done later with track_iter.
+        
+        Non-priority (at the moment) but will be needed later:
+        - get_instructions .positions needs implementing for versions < 3.11 if possible (might not be)
+        - check track_iter and probably add more conditional checks?
+        - check all the documentation + docstrings
+        - f_back on frame needs checking
+        - check lineno_adjust to ensure that it's robust, not sure if it works in all cases.
+          It relies on a single line containing all the code, it might be possible that you
+          can have multiple independent expressions in one line but I haven't checked. -
+          This function is only to help with users that choose to use compound statements.
 
     Expansion to other types of generators:
 
