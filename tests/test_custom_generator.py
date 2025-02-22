a179a3eb70080b13d7e6bf6ce9ad3ddd717c2b71
Benj1bear <131564813+Benj1bear@users.noreply.github.com>
2025-02-22 22:09:47 +1300

    Update test_custom_generator.py

--- tests/test_custom_generator.py
+++ tests/test_custom_generator.py
@@ -30,13 +30,31 @@ def test_Pickler(pickler_test: Pickler = None) -> None:
             )
 
 
-def test_code_and_frame_pickler() -> None:
+def test_picklers() -> None:
     _frame = frame(currentframe())
     _code = code(_frame.f_code)
     test_Pickler(_code)
     ## probably needs more work before f_back can be pickled ##
     _frame.f_back = None
     test_Pickler(_frame)
+    test_Pickler(Generator())
+
+
+def test_generator_custom_adjustment() -> None:
+    gen = Generator()
+    test = lambda line: gen._custom_adjustment(line, 0)
+    ## yield ##
+    assert test("yield ... ") == ["return ... "]
+    ## yield from ##
+    assert test("yield from ... ") == ["locals()['.yieldfrom']=... ", "for locals()['.i'] in locals()['.yieldfrom']:", "    return locals()['.i']"]
+    ## for/while positions + default case return ##
+    gen._internals["jump_positions"], gen._internals["jump_stack"] = [], []
+    assert test("for ") == ["for "]
+    assert gen._internals["jump_positions"], gen._internals["jump_stack"] == ([[0, None]], [(0, 0)])
+    assert test("while ") == ["while "]
+    assert gen._internals["jump_positions"], gen._internals["jump_stack"] == ([[0, None], [0, None]], [(0, 0), (0, 1)])
+    ## return ##
+    assert test("return ... ") == ['try:', '    raise StopIteration(... )', 'finally:', "    currentframe().f_back.f_locals['self'].close()"]
 
 
 def test_generator_update_jump_positions() -> None:
@@ -47,7 +65,11 @@ def test_generator_append_line() -> None:
     pass
 
 
-def test_generator_custom_adjustment() -> None:
+def test_generator_block_adjust() -> None:
+    pass
+
+
+def test_generator_string_collector_adjust() -> None:
     pass
 
 
@@ -55,7 +77,35 @@ def test_generator_clean_source_lines() -> None:
     pass
 
 
+def test_generator_create_state() -> None:
+    pass
+
+def test_generator_init_states() -> None:
+    pass
+
+
 def test_generator__init__() -> None:
+    pass
+
+
+def test_generator_frame_init() -> None:
+    gen = Generator()
+    gen._internals["frame"] = frame()
+    assert gen._frame_init() == """def next_state():
+    locals=currentframe().f_back.f_locals['self']._locals
+    currentframe().f_back.f_locals['.frame']=currentframe()
+"""
+    gen._internals["frame"].f_locals.update({"a":3,"b":2,"c":1})
+    assert gen._frame_init() == """def next_state():
+    locals=currentframe().f_back.f_locals['self']._locals
+    a=locals()[a]
+    b=locals()[b]
+    c=locals()[c]
+    currentframe().f_back.f_locals['.frame']=currentframe()
+"""
+
+
+def test_generator_update() -> None:
     pass
 
 
@@ -67,13 +117,56 @@ def test_generator__iter___() -> None:
     pass
 
 
+def test_generator_close() -> None:
+    gen = Generator()
+    gen._internals = {}
+    assert gen.close() is None
+    for key, value in {
+                "frame": None,
+                "running": False,
+                "suspended": False,
+                "yieldfrom": None,
+            }.items():
+        assert gen._internals[key] == value
+    count = 0
+    for i in gen._internals["state_generator"]:
+        count += 1
+    assert count == 0
+
+def test_generator_send() -> None:
+    pass
+
+def test_generator_throw() -> None:
+    pass
+
+
 def test_generator_type_checking() -> None:
+    gen = Generator()
+    assert isinstance(gen, (GeneratorType, Generator)) and issubclass(type(gen), (GeneratorType, Generator))
+
+
+def test_generator__len___() -> None:
     pass
 
 
-def test_generator_pickler() -> None:
-    pass
-
-
+## tests are for cleaning + adjusting + pickling ##
 test_Pickler()
-test_code_and_frame_pickler()
+# test_picklers()
+test_generator_custom_adjustment()
+# test_generator_update_jump_positions()
+# test_generator_append_line()
+# test_generator_block_adjust()
+# test_generator_string_collector_adjust()
+# test_generator_clean_source_lines()
+# test_generator_create_state()
+# test_generator_init_states()
+# test_generator__init__()
+test_generator_frame_init()
+# test_generator_update()
+# test_generator__next___()
+# test_generator__iter___()
+test_generator_close()
+# test_generator_send()
+# test_generator_throw()
+test_generator_type_checking()
+# test_generator__len__()
