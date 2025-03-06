f3dfb1ae776e778c5914aef2b29172ce8e9e9a2d
Benj1bear <131564813+Benj1bear@users.noreply.github.com>
2025-03-07 10:37:54 +1300

    Update comments.py

--- gcopy/comments.py
+++ gcopy/comments.py
@@ -1,16 +1,14 @@
 """
 TODO:
 
-1. general testing and fixing to make sure everything works before any more changes are made
-
     Finish fixing:
 
-        Priority (in order):
-        --------------------
-        - custom_generator -
-        --------------------
-          _clean_source_lines
-        
+         _clean_source_lines:
+
+         - Need to check blocks + named statements with value yields
+         - indentation + except adjust needs its indentation fixed
+         - Docstring is not quite correct by ending with newlines (doesn't really matter though)
+
         Non-priority (at the moment) but will be needed later:
         - When do i.e. gi_running and gi_suspended change?
         - check all the documentation + docstrings
@@ -19,13 +17,13 @@ TODO:
     Expansion to other types of generators:
 
      - Add some async features to AsyncGenerator - will need to work out how I want to do the async stuff
-       async has renamings of the dunders as well i.e. methods of interest are __iter__ is __aiter__, __next__ 
+       async has renamings of the dunders as well i.e. methods of interest are __iter__ is __aiter__, __next__
        is __anext__, and it will probably need an __await__ implementation.
 
      - Consider generator functions decorated with types.coroutine or if making a coroutine type is necessary
 
      - Maybe need to make an internal generator and then use this generally?
-     
-  - Make a Note in the documentation that while loops don't need tracking and 
+
+  - Make a Note in the documentation that while loops don't need tracking and
     indentation is enough as an identifier for tracking
 """
