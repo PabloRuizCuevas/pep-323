b7369397bdb747bc22bf149e2530a78138eb5893
Benj1bear <131564813+Benj1bear@users.noreply.github.com>
2025-03-10 19:29:47 +1300

    Update comments.py

--- gcopy/comments.py
+++ gcopy/comments.py
@@ -3,16 +3,32 @@ TODO:
 
     Finish fixing:
 
-         _clean_source_lines:
+        _clean_source_lines (testing):
 
-         - Need to check blocks + named statements with value yields
-         - indentation + except adjust needs its indentation fixed
-         - Docstring is not quite correct by ending with newlines (doesn't really matter though)
+         - check _block_adjust
+         - check the resets in variables in _clean_source_lines
+           (i.e. after 'yield' and string_collector_adjust)
+         - test collect_lambda
+         - test the decorator/definition adjustments
+         - test ternary statements
+
+        unpack:
+         - ternary statements with and without value yields
+         - check collect_lambda
+
+        - fix up tests from new implementations
+
+        Other testing:
+        Make sure track_iter is tested in application with generators and generator expresssions
 
         Non-priority (at the moment) but will be needed later:
         - When do i.e. gi_running and gi_suspended change?
         - check all the documentation + docstrings
-        - f_back on frame needs checking
+        - After stopIteration the frame should be None
+        - remove any unncesesary code, comments etc.
+        - consider unpacking ternary statements in generator expressions, will have to for lambdas
+        - maybe export all the cleaning stuff to a cleaner class so the Generator class is
+          only Generator related. Same might be desired with the code adjustments maybe
 
     Expansion to other types of generators:
 
@@ -26,4 +42,14 @@ TODO:
 
   - Make a Note in the documentation that while loops don't need tracking and
     indentation is enough as an identifier for tracking
+
+  - Make a note in the documentation that yields in comprehension expressions and exec/eval don't occur
+    in python syntax
+
+  - write in the documentation that there is a .internal and it has 'args', 'yieldfrom', 'send',
+   'exec_info', .decorator, 'partial', '.continue', '.i', '.error', and the tracking variables (indents e.g. '.4' etc.)
+
+    at the moment only 'partial', 'exec_info', '.args', and '.send' are initialized with '.send'
+    being initialized with 'None' every iteration except when the user sends an argument
+
 """
