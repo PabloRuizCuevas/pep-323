665221fca5804e3a9bfce76d1a55ea31dc12ab01
Benj1bear <131564813+Benj1bear@users.noreply.github.com>
2025-03-16 14:25:54 +1300

    Update comments.py

--- gcopy/comments.py
+++ gcopy/comments.py
@@ -1,6 +1,18 @@
 """
 TODO:
 
+  1. implement ternary statement handling in unpack
+  2. finish testing
+
+
+    Currently not working (_clean_source_lines):
+     - some value yields in f-strings in statements
+     - ternary expressions with and without value yields
+     - the decorator/definition adjustments needs testing
+     - collect_lambda needs testing
+     - a colon is added in a new line instead of the current
+       line for while loops adjusted by _block_adjust or unpack
+
     Finish fixing:
 
         _clean_source_lines (testing):
@@ -11,12 +23,14 @@ TODO:
          - test collect_lambda
          - test the decorator/definition adjustments
          - test ternary statements
+         - test cleaning of sourcelines for lambda expressions
 
         unpack:
          - ternary statements with and without value yields
          - check collect_lambda
 
-        - fix up tests from new implementations
+      - reseting a generator is possible but no gurantees are made on saving the initial f_locals
+        including nonlocals e.g. closure cells. - maybe copy over the __closure__ cells
 
         Other testing:
         Make sure track_iter is tested in application with generators and generator expresssions
@@ -24,11 +38,10 @@ TODO:
         Non-priority (at the moment) but will be needed later:
         - When do i.e. gi_running and gi_suspended change?
         - check all the documentation + docstrings
-        - After stopIteration the frame should be None
         - remove any unncesesary code, comments etc.
-        - consider unpacking ternary statements in generator expressions, will have to for lambdas
         - maybe export all the cleaning stuff to a cleaner class so the Generator class is
           only Generator related. Same might be desired with the code adjustments maybe
+
 
     Expansion to other types of generators:
 
@@ -39,6 +52,8 @@ TODO:
      - Consider generator functions decorated with types.coroutine or if making a coroutine type is necessary
 
      - Maybe need to make an internal generator and then use this generally?
+
+  Documentation notes:
 
   - Make a Note in the documentation that while loops don't need tracking and
     indentation is enough as an identifier for tracking
@@ -51,5 +66,15 @@ TODO:
 
     at the moment only 'partial', 'exec_info', '.args', and '.send' are initialized with '.send'
     being initialized with 'None' every iteration except when the user sends an argument
+
+  - I've decided that we don't record f_back for each state since if you really need
+    to record the states then you should do that separately before/after each state is
+    used because it's generally considered better that way, the bigger downside is the
+    memory consumption of saving all the states as f_back on frames when really we're
+    usually only interested in it running and it's current state. The previous states
+    variables should be the current versions and the linenos can be recorded in between
+    states easily. The other exploration one could do is rerunning states again but why
+    not just copy the generator then (as this software was designed for)? So it seems
+    unnecessary to save as f_back.
 
 """
