2740d28ce4f52dea182af88f18470eb96059cec9
Benj1bear <131564813+Benj1bear@users.noreply.github.com>
2025-03-16 21:41:15 +1300

    Update source_processing.py

--- gcopy/source_processing.py
+++ gcopy/source_processing.py
@@ -1449,3 +1449,485 @@ def sign(
     temp.__source__ = source
     temp.__doc__ = FUNC2.__doc__
     return temp
+
+
+######################################################
+#### Cleaning and adjusting Generator source code ####
+######################################################
+
+
+def record_jumps(self, number_of_indents: int) -> None:
+    """Records the jump positions for the loops (for and while) to help with code adjustments"""
+    ## has to be a list since we're assigning ##
+    self._internals["jump_positions"] += [[self._internals["lineno"], None]]
+    self._internals["jump_stack"] += [
+        (number_of_indents, len(self._internals["jump_positions"]) - 1)
+    ]
+
+
+def custom_adjustment(self, line: str, number_of_indents: int = 0) -> list[str]:
+    """
+    It does the following to the source lines:
+
+    1. replace all lines that start with yields with returns to start with
+    2. make sure the generator is closed on regular returns
+    3. save the iterator from the for loops replacing with a nonlocal variation
+    4. tend to all yield from ... with the same for loop variation
+    5. adjust all value yields either via unwrapping or unpacking
+    """
+    temp_line = line[number_of_indents:]
+    indent = " " * number_of_indents
+    ## decorator ##
+    if temp_line.startswith("@"):
+        self._internals["decorator"] = True
+        return [line]
+    ## yield ##
+    result = yield_adjust(temp_line, indent)
+    if result is not None:
+        return result
+    ## loops ##
+    if is_loop(temp_line):
+        record_jumps(self, number_of_indents)
+        return [line]
+    ## return ##
+    if temp_line == "return" or temp_line.startswith("return "):
+        ## close the generator then return ##
+        ## have to use a try-finally in case the user returns a value from locals() ##
+        return [indent + "return EOF('" + temp_line[7:] + "')"]
+    ## nonlocal ##
+    if temp_line.startswith("nonlocal "):
+        ## we have to remove the nonlocal keyword since there will be no bindings. ##
+        ## The args are collected in __init__ for unintialized function generators; ##
+        ## initialized generators already have them in their locals ##
+        self._internals["lineno"] -= 1
+        return []
+    return [line]
+
+
+def update_jump_positions(self, lines: list[str], reference_indent: int = -1) -> None:
+    """
+    Updates the end jump positions in self._internals["jump_positions"].
+    It may also append the current lines with adjustments if it's a while
+    loop that used a value yield in its condition
+    """
+    if self._internals["jump_stack"]:
+        end_lineno = self._internals["lineno"]
+        while (
+            self._internals["jump_stack"]
+            and reference_indent <= self._internals["jump_stack"][-1][0]
+        ):  # -1: top of stack, 0: indent
+            index = self._internals["jump_stack"].pop()[1]
+            self._internals["jump_positions"][index][1] = end_lineno
+            ## add the adjustments
+            if (
+                self._internals["jump_stack_adjuster"]
+                ## check if they're the same lineno ##
+                and self._internals["jump_positions"][index][0]
+                == self._internals["jump_stack_adjuster"][-1][0]
+            ):
+                adjustments = self._internals["jump_stack_adjuster"].pop().pop()
+                ## add the adjustments ##
+                lines += adjustments
+                ## make sure with the adjustments that have loops ##
+                ## that the loops are recorded and the lineno is adjusted ##
+                count = 0
+                for self._internals["lineno"], line in enumerate(
+                    ## temporarily add an additional adjustment to make ##
+                    ## sure the loop positions are recorded properly ##
+                    adjustments + [""],
+                    start=self._internals["lineno"] + 1,
+                ):
+                    number_of_indents = get_indent(line)
+                    if is_loop(line[number_of_indents:]):
+                        count += 1
+                        record_jumps(self, number_of_indents)
+                    ## we can use -1 since all the loops should be complete ##
+                    elif (
+                        count
+                        and number_of_indents <= self._internals["jump_stack"][-1][0]
+                    ):
+                        index = self._internals["jump_stack"].pop()[1]
+                        self._internals["jump_positions"][index][1] = (
+                            self._internals["lineno"] + 1
+                        )
+                    self._internals["linetable"] += [self._internals["lineno"]]
+                ## since we temporarily increased the number of adjustments ##
+                self._internals["lineno"] -= 1
+                self._internals["linetable"].pop()
+    return lines
+
+
+def append_line(
+    self,
+    index: int,
+    char: str,
+    source: str,
+    source_iter: Iterable,
+    running: bool,
+    line: str,
+    lines: list[str],
+    indentation: int,
+    indent_adjust: int,
+) -> tuple[int, str, int, list[str], str, bool, int]:
+    ## skip comments ##
+    if char == "#":
+        for index, char in source_iter:
+            if char == "\n":
+                break
+    ## make sure to include it ##
+    if char == ":":
+        indentation = get_indent(line) + 4  # in case of ';'
+        line += char
+    if line and not line.isspace():  ## empty lines are possible ##
+        reference_indent = get_indent(line)
+        ## make sure the line is corrected if it currently assumes indentation when it shouldn't ##
+        if (
+            indent_adjust
+            and reference_indent - (char == ":") * 4 >= indentation - indent_adjust
+        ):
+            line = " " * indent_adjust + line
+        else:
+            indent_adjust = 0
+        lines = update_jump_positions(self, lines, reference_indent)
+        ## skip the definitions ##
+        temp_line = line[reference_indent:]
+        if is_definition(temp_line):
+            index, char, self._internals["lineno"], lines = collect_definition(
+                index - len(line) + 1,
+                lines,
+                self._internals["lineno"],
+                source,
+                source_iter,
+                reference_indent,
+                self._internals["decorator"],
+            )
+            self._internals["decorator"] = False
+        else:
+            ## update the lineno before so the loops positions are lineno based ##
+            self._internals["lineno"] += 1
+            lines += custom_adjustment(self, line, reference_indent)
+            ## make a linetable if using a running generator ##
+            ## for the linetable e.g. for lineno_adjust e.g. compound statements ##
+            # if running and char == "\n":
+            #     self._internals["linetable"] += [self._internals["lineno"]]
+        line, indented = setup_next_line(char, indentation)
+    else:
+        reference_indent = indentation
+        line, indented = "", False
+
+    # if indent_adjust:
+    #     line += " " * indent_adjust
+    ## start a new line ##
+    return index, char, lines, line, indented, indentation, indent_adjust
+
+
+def block_adjust(
+    self,
+    current_lines: list[str],
+    new_lines: list[str],
+    final_line: str,
+    source: str,
+    source_iter: Iterable,
+) -> list[str]:
+    """
+    Checks if lines that were adjusted because of value yields
+    are in a block statement and therefore needs adjusting
+
+    Also, the new_lines do need to be indented accordingly
+    e.g. to the final_line or specfic adjustment
+    """
+    ## make sure any loops are recorded (necessary for 'yield from ...' adjustment) ##
+    ## and the lineno is updated ##
+
+    for self._internals["lineno"], line in enumerate(
+        new_lines, start=self._internals["lineno"] + 1
+    ):
+        number_of_indents = get_indent(line)
+        if is_loop(line[number_of_indents:]):
+            record_jumps(self, self._internals["lineno"], number_of_indents)
+    ## check for adjustments in the final line ##
+    number_of_indents = get_indent(final_line)
+    temp_line = final_line[number_of_indents:]
+    check = lambda expr: temp_line.startswith(expr) and temp_line[len(expr)] in " :"
+    if temp_line[:1] == "@":
+        temp_line = temp_line[1:]
+        signature = get_signature(temp_line, True)
+        if signature[1]:
+            temp_line = "locals()['.internals']['partial'](%s, %s)" % signature
+        else:
+            temp_line = signature[0]
+        if self._internals["decorator"]:
+            temp_line = "locals()['.internals']['.decorator'](%s)" % temp_line
+        else:
+            self._internals["decorator"] = True
+        final_line = (
+            " " * number_of_indents
+            + "locals()['.internals']['.decorator'] = "
+            + temp_line
+        )
+    elif is_definition(temp_line):
+        index, char, self._internals["lineno"], lines = collect_definition(
+            index - len(line) + 1,
+            lines,
+            self._internals["lineno"],
+            source,
+            source_iter,
+            number_of_indents,
+            self._internals["decorator"],
+        )
+        self._internals["decorator"] = False
+    elif is_loop(temp_line):
+        ## the end of the loop needs to be appended with the new_lines ##
+        ## locals()[".args"] += next(...)
+        ## while locals()[".args"].pop():
+        ##     ...
+        ##     locals()[".args"] += next(...)
+        self._internals["jump_stack_adjuster"] += [
+            [self._internals["lineno"]] + indent_lines(new_lines, number_of_indents + 4)
+        ]
+    ## needs to indent itself and all other lines until the end of the block ##
+    ## Note: only elif since if statements should be fine ##
+    elif check("elif"):
+        ## +4 to encapsulate in an else statement +2 to make it an 'if' statement ##
+        final_line = " " * (number_of_indents + 4) + final_line[number_of_indents + 2 :]
+        return (
+            current_lines
+            + [" " * number_of_indents + "else:"]
+            + indent_lines(new_lines, number_of_indents + 4)
+            + [final_line]
+        )
+    elif check("except"):
+        ## this is the problem ##
+        # if current_lines[-2] == ' try:':
+        #     current_lines[-2] = ' ' * 8 + 'try:'
+        #     final_line = final_line[8:]
+        ## except_adjust automatically does the indentation ##
+        return except_adjust(current_lines, new_lines, final_line)
+    self._internals["lineno"] += 1
+    return current_lines + indent_lines(new_lines, number_of_indents) + [final_line]
+
+
+def string_collector_adjust(
+    self,
+    index: int,
+    char: str,
+    prev: tuple[int, int, str],
+    source_iter: Iterable,
+    line: str,
+    source: str,
+    lines: list[str],
+) -> tuple[str, int, list[str]]:
+    """Adjust the string collector in case of any value yields in the f-strings"""
+    string_collected, prev, adjustments = string_collector_proxy(
+        index, char, prev, source_iter, line, source
+    )
+    if adjustments:
+        ## since we have adjustments we need to adjust the chars before it ##
+        adjustments_start, line_start, index = unpack(line, source=source, index=index)
+        line_start = " " * get_indent(line) + line_start.lstrip()
+        adjustments_end, line_end, index = unpack(
+            source_iter=source_iter, source=source, index=index
+        )
+        final_adjustments, final_line = (
+            adjustments_start + adjustments + adjustments_end,
+            line_start + string_collected + line_end,
+        )
+        return (
+            index,
+            prev,
+            block_adjust(
+                self, lines, final_adjustments, final_line, source, source_iter
+            ),
+        )
+    return line + string_collected, prev, lines
+
+
+def clean_source_lines(self, running: bool = False) -> list[str]:
+    """
+    source: str
+
+    returns source_lines: list[str],return_linenos: list[int]
+
+    1. fixes any indentation issues (when ';' is used) and skips empty lines
+    2. split on "\n", ";", and ":"
+    3. join up the line continuations i.e. "\ ... " will be skipped
+
+    additionally, custom_adjustment will be called on each line formation as well
+
+    Note:
+    jump_positions: are the fixed list of (lineno,end_lineno) for the loops (for and while)
+    jump_stack: jump_positions currently being recorded (gets popped into jump_positions once
+                    the reference indent has been met or lower for the next line that does so)
+                    it records a tuple of (reference_indent,jump_position_index)
+    """
+    ## for loop adjustments ##
+    (
+        self._internals["jump_positions"],
+        self._internals["jump_stack"],
+        self._internals["jump_stack_adjuster"],
+        self._internals["decorator"],
+        self._internals["lineno"],
+    ) = (
+        [],
+        [],
+        [],
+        False,
+        0,
+    )
+    ## setup source as an iterator and making sure the first indentation's correct ##
+    source = skip_source_definition(self._internals["source"])
+    ## we need to make sure the source is saved for skipping for line continuations ##
+    source = source[get_indent(source) :]
+    source_iter = enumerate(source)
+    ID, depth, line, lines, indented, space, indentation, prev, indent_adjust = (
+        "",
+        0,
+        " " * 4,
+        [],
+        False,
+        0,
+        4,
+        (0, 0, ""),
+        0,
+    )
+    ## enumerate since I want the loop to use an iterator but the
+    ## index is needed to retain it for when it's used on get_indent
+    for index, char in source_iter:
+        ## collect strings ##
+        if char == "'" or char == '"':
+            line, prev, lines = string_collector_adjust(
+                self, index, char, prev, source_iter, line, source, lines
+            )
+            if isinstance(line, int):
+                depth, ID = 0, ""
+                indentation = get_indent(lines[-1])
+                line, indented = setup_next_line(source[line], indentation)
+                space = index
+        ## makes the line singly spaced while retaining the indentation ##
+        elif char == " ":
+            line, space, indented = singly_space(index, char, line, space, indented)
+        ## join everything after the line continuation until the next \n or ; ##
+        elif char == "\\":
+            skip_line_continuation(source_iter, source, index)
+            ## in case of a line continuation without a space before (whitespace after is removed) ##
+            ## Note: i.e. 'func        ()' is valid in python ##
+            if space + 1 != index:
+                line += " "
+                space = index
+        ## create new line ##
+        ## should be able to go depth == 0 ... like in unpack ##
+        elif char in "#\n;" or (char == ":" and source[index + 1 : index + 2] != "="):
+            ## 'space' is important (otherwise we get more indents than necessary) ##
+            space, char, lines, line, indented, indentation, indent_adjust = (
+                append_line(
+                    self,
+                    index,
+                    char,
+                    source,
+                    source_iter,
+                    running,
+                    line,
+                    lines,
+                    indentation,
+                    indent_adjust,
+                )
+            )
+            depth, ID = 0, ""
+        else:
+
+            ### might move this section into a function ###
+
+            line += char
+            ## detect value yields [yield] and {yield} is not possible only (yield) ##
+            depth = update_depth(depth, char)
+            if char == "=":  ## '... = yield ...' and '... = yield from ...'
+                depth += 1
+            if depth and char.isalnum():
+                ID, line, lines, indented, index, char, space = source_update_ID(
+                    self,
+                    source,
+                    source_iter,
+                    prev,
+                    space,
+                    index,
+                    char,
+                    ## : index + 2 in case of IndexError ##
+                    source[index + 1 : index + 2],
+                    ID,
+                    line,
+                    lines,
+                    indented,
+                )
+            else:
+                ID = ""
+    ## in case you get a for loop at the end and you haven't got the end jump_position ##
+    ## then you just pop them all off as being the same end_lineno ##
+    ## note: we don't need a reference indent since the line is over e.g. ##
+    ## the jump_stack will be popped if it exists ##
+    lines = update_jump_positions(self, lines)
+    ## are no longer needed ##
+    del (
+        self._internals["jump_stack"],
+        self._internals["jump_stack_adjuster"],
+        self._internals["decorator"],
+    )
+    return lines
+
+
+def source_update_ID(
+    self,
+    source,
+    source_iter,
+    prev,
+    space,
+    index,
+    char,
+    next_char,
+    ID,
+    line,
+    lines,
+    indented,
+) -> tuple:
+    ## in case of ... ... (otherwise you keep appending the ID) ##
+    if space + 1 == index:
+        ID = ""
+    ID += char
+    if ID == "lambda" and next_char in " :\\":
+        char, lines = collect_lambda(line, source_iter, source, prev)
+        indentation = get_indent(line)
+        line, indented = setup_next_line(char, indentation)
+        self._internals["lineno"] += 1
+        ID = ""
+    elif ID == "yield" and next_char in " )]}\n;\\":
+        new_lines, final_line, index = unpack(
+            line, source_iter, source=source, index=index
+        )
+        final_line = " " * get_indent(line) + final_line.lstrip()
+        if line[get_indent(line) :].startswith("elif "):
+            indent_adjust = 4
+        lines = block_adjust(self, lines, new_lines, final_line, source, source_iter)
+        ## for the linetable e.g. for lineno_adjust e.g. compound statements ##
+        ## isn't temp == self._internals["lineno"]? ##
+        # temp = len(lines)
+        # if running:
+        #     self._internals["linetable"] += [
+        #         self._internals["lineno"]
+        #     ] * (len(lines) - temp)
+
+        if is_definition(final_line[get_indent(final_line) :]):
+            line, indented = "", False
+        else:
+            indentation = get_indent(lines[-1]) + indent_adjust
+            line, indented = setup_next_line(source[index], indentation)
+        if indent_adjust:
+            line = " " * indentation
+            space = index
+        ID = ""
+    ## ternary statement ##
+    elif ID == "if" and next_char in " \\" and line[:-2].lstrip():
+        new_lines, final_line, _ = unpack(line, source_iter, source=source, index=index)
+        lines = self.block_adjust(
+            self, lines, new_lines, final_line, source, source_iter
+        )
+        ## setup for a new line ##
+    return ID, line, lines, indented, index, char, space
